eceee1308afbf89f3da84aba726519cbfe34666f
Teddy Bandama <tebandam@1G1.42angouleme.fr>
2023-11-13 12:21:33 +0100

    add read

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 11:12:38 by tebandam          #+#    #+#             */
-/*   Updated: 2023/11/13 11:32:48 by tebandam         ###   ########.fr       */
+/*   Updated: 2023/11/13 12:20:24 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,31 @@
 #include <stdio.h>
 #define BUFF_SIZE 32
 
-int get_next_line(const int fd, char **line)
+//int get_next_line(const int fd, char **line)
+//{
+    
+//}
 
 
+int main(void)
+{
+    int fd;
+    int i;
+    int stock;
+    char    buff[BUFF_SIZE];
 
+    i = 0;
+    fd = open("example.txt", O_RDWR);
+    
+    while (buff[BUFF_SIZE] != '\n')
+    {
+        stock = read(fd, buff + i, 1);
+        i++;
+    }
+    printf("content: %s", buff);
+    close(fd);
+    return (0);
+}
 
 
 /*
