5646f24132a60e4371b8990ef5af5121c1ea064a
Teddy Bandama <tebandam@2B4.42angouleme.fr>
2023-11-17 10:19:00 +0100

    add procted for prev_read

--- get_next_line.c
+++ get_next_line.c
@@ -1,81 +1,5 @@
-#include <unistd.h>
-#define BUFFER_SIZE 6
-#include <fcntl.h>
-#include <stdlib.h>
 #include "get_next_line.h"
-
-int ft_strlen(const char *str)
-{
-    int  i;
-
-	if (!str)
-		return (0);
-    i = 0;
-    while (str[i])
-        i++;
-    return (i);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == (char)c)
-			return ((char *)(&s[i]));
-		i++; 
-	}
-	if (s[i] == (char)c)
-		return ((char *)(&s[i]));
-	return (NULL);
-}
-
-char	*ft_strdup(const char *s)
-{
-	char		*tab;
-	int			i;
-
-	i = 0;
-	tab = malloc(sizeof(char) * ft_strlen(s) + 1);
-	if (tab)
-	{
-		while (s[i])
-		{
-			tab[i] = s[i];
-			i++;
-		}
-		tab[i] = '\0';
-	}
-	return ((char *)tab);
-}
-
-char	*ft_substr(char const *s, unsigned int start, int len)
-{
-	int	i;
-	int	j;
-	char	*tab= NULL;
-
-	if (!s)
-		return (NULL);
-	if ((unsigned int)ft_strlen(s) < start)
-		return (ft_strdup(""));
-	j = ft_strlen(s + start);
-	if (j < len)
-		len = j;
-	tab = malloc(sizeof(char) * (len + 1));
-	if (!tab)
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		tab[i] = s[start + i];
-		i++;
-	}
-	tab[i] = '\0';
-	return (tab);
-}
+#define BUFFER_SIZE 6
 
 char	*ft_strjoin(char *s1, char *s2)
 {
@@ -107,7 +31,7 @@ char	*ft_strjoin(char *s1, char *s2)
     return (tab);
 }
 
-char *read_line(int fd)
+char *read_line(int fd, char *prev_read)
 {
     int len;
     char    buf[BUFFER_SIZE];
@@ -116,7 +40,13 @@ char *read_line(int fd)
 	// verif il y a rien dans stock
 
     len = 1;
-    stock = ft_strdup("");
+    if (prev_read)
+	{
+		stock = ft_strdup(prev_read);
+		free(prev_read);
+	}	
+	else
+		stock = ft_strdup("");
     while (len > 0 && ft_strchr(stock, '\n') == NULL)
     {
         len = read(fd, buf, BUFFER_SIZE);
@@ -160,7 +90,7 @@ char *get_next_line(int fd)
     
 	if (fd == -1 || BUFFER_SIZE <= 0) 
 	    return (NULL);
-    stock = read_line(fd);
+    stock = read_line(fd, stock);
 	line = extract_line(stock);
 	stock = extract_surplus_line(stock);
 	return (line);
@@ -173,7 +103,11 @@ int main(void)
     static char    *str;
     
     fd = open("example.txt", O_RDONLY);
-    str = get_next_line(fd);
-    printf("%s", str);
+    do{
+		str = get_next_line(fd);
+    	printf("%s", str);
+
+	}
 	free(str);
+	close(fd);
 }
---
+++ get_next_line_utils.c
@@ -0,0 +1,74 @@
+#include "get_next_line.h"
+
+int ft_strlen(const char *str)
+{
+    int  i;
+
+	if (!str)
+		return (0);
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)(&s[i]));
+		i++; 
+	}
+	if (s[i] == (char)c)
+		return ((char *)(&s[i]));
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char		*tab;
+	int			i;
+
+	i = 0;
+	tab = malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (tab)
+	{
+		while (s[i])
+		{
+			tab[i] = s[i];
+			i++;
+		}
+		tab[i] = '\0';
+	}
+	return ((char *)tab);
+}
+
+char	*ft_substr(char const *s, unsigned int start, int len)
+{
+	int	i;
+	int	j;
+	char	*tab= NULL;
+
+	if (!s)
+		return (NULL);
+	if ((unsigned int)ft_strlen(s) < start)
+		return (ft_strdup(""));
+	j = ft_strlen(s + start);
+	if (j < len)
+		len = j;
+	tab = malloc(sizeof(char) * (len + 1));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		tab[i] = s[start + i];
+		i++;
+	}
+	tab[i] = '\0';
+	return (tab);
+}
