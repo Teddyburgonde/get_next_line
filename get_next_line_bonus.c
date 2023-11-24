bf45ba6577a950aec48cb10c597e45a3e37eb3d2
Teddy Bandama <tebandam@1E7.42angouleme.fr>
2023-11-24 16:52:50 +0100

    project valide 100% if re push i must have 125%

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static char	*read_loop(char *buf, char *stock, int *len, int fd)
 {
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_BONUS_H 
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5
+#  define BUFFER_SIZE 1
 # endif
 
 # include <unistd.h>
